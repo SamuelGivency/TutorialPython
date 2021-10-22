9e6768b7e696fd88dfb45710cb458a1dff907fa3
SamuelGivency <givencysamuel1@gmail.com>
2021-10-22 19:34:21 +0700

    menambahkan file python

---
+++ tugas_python.py
@@ -0,0 +1,14 @@
+print("hello");
+print()
+nama = input ("Nama: ");
+panggilan = input ("Panggilan: ");
+nim = input("Nim: ");
+tgl = input ("Tanggal Lahir: ");
+tmpt= input ("tempat lahir: ");
+hp = input ("No Hp: ");
+alamat = input("Alamat: ");
+umur = input ("Umur: ");
+print()
+print("Let me introduce my self, may name is",nama,"but you can call me",panggilan,
+      "My NIM is",nim,"I was born in",tmpt,"I am",umur,"years old. I am very glad"
+      " So, dont forget to call me before with the number",hp,);
